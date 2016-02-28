dd8e88904e24f7802c77197d00a933a37e3b9d2d
Isac Arnekvist <isacar@kth.se>
2016-02-28 10:50:40 +0100

    4 initial views

--- src/web/views.py
+++ src/web/views.py
@@ -3,4 +3,30 @@ from django.http import HttpResponse
 
 # Create your views here.
 def frontpage(request):
-    return render(request, 'web/frontpage.html')
+    ctx = {
+        'home_active': True,
+    }
+    return render(request, 'web/frontpage.html', context=ctx)
+
+
+def rooms(request):
+    ctx = {
+        'rooms_active': True,
+    }
+    return render(request, 'web/rooms.html', context=ctx)
+
+
+def blog(request):
+    ctx = {
+        'blog_active': True,
+    }
+    return render(request, 'web/blog.html', context=ctx)
+
+
+def booking(request):
+    ctx = {
+        'booking_active': True,
+    }
+    return render(request, 'web/booking.html', context=ctx)
+
+
