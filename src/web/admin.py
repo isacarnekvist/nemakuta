c75852cc5b92c0ed3ee08830fd9f2a0dc154cc12
Isac Arnekvist <isacar@kth.se>
2016-03-06 12:46:07 +0100

    Houses model

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from web.models import House
 
 # Register your models here.
+admin.site.register(House)
--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    name = models.CharField(help_text='T ex "Round House 1"', max_length=128)
+    desc = models.TextField(help_text='T ex "This is the largest..."')
+    min_pers_capacity = models.SmallIntegerField(
+                                help_text='Hur många är minsta lämpliga ' +
+                                          'antalet att bo i huset? T ex 1')
+    max_pers_capacity = models.IntegerField(
+                                help_text='Hur många kan som mest sova i ' +
+                                          'huset? T ex 10')
+    n_rooms = models.SmallIntegerField(help_text="Hur många rum har huset?")
+    n_doubles = models.SmallIntegerField(help_text="Hur många dubbelsängar?")
+    n_singles = models.SmallIntegerField(help_text="Hur många enkelsängar?")
+    img_path = models.CharField(help_text="Sökvägen till bilden",
+                                max_length=128)
+    tripadvisor_book_link = models.URLField(
+                                help_text='Länk till tripadvisors ' +
+                                          'bokningssida')
+
+    def __repr__(self):
+        return 'House: {}'.format(self.name)
+
+    def __str__(self):
+        return self.__repr__()
